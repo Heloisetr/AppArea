3e0120b13320b76c28cf9052577ae6af6b23668e
Louis Beugnon <louis.beugnon@epitech.eu>
2020-02-17 16:04:26 +0100

    push of API call with fetch in Homepage.js

--- areaMobile/Scenes/HomePage.js
+++ areaMobile/Scenes/HomePage.js
@@ -1,28 +1,59 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View, ImageBackground, TextInput, Dimensions, TouchableOpacity, Image } from 'react-native';
 
-
 export default class HomePage extends Component
 {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true,
+            dataSource: null,
+        }
+    }
+
+    componentDidMount () {
+
+        return fetch('https://api.openweathermap.org/data/2.5/find?q=Bordeaux&units=imperial&appid=f92c1f4990b0574d4a4e4d3dd556f388')
+            .then ((response) => response.json())
+            .then ((responseJson) => {
+                this.setState({
+                    isLoading: false,
+                    dataSource: responseJson.list[0],
+                })
+            })
+            .catch((error) => {
+                console.log(error)
+            });
+
+    }
+
     render() {
-        const data = {
-            labels: ["Swim", "Bike", "Run"],
-            data: [0.4, 0.6, 0.8]
-        };
-        /*const chartConfig = {
-            backgroundGradientFrom: "#1E2923",
-            backgroundGradientFromOpacity: 0,
-            backgroundGradientTo: "#08130D",
-            backgroundGradientToOpacity: 0.5,
-            color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-            strokeWidth: 2, // optional, default 3
-            barPercentage: 0.5
-          };*/
+
+        if (this.state.isLoading) {
+            return (
+                <View>
+                    <Text>Loading</Text>
+                </View>
+            )
+        } else {
+            let temp = this.state.dataSource.main.temp;
+            let humidity = this.state.dataSource.main.humidity;
+            /*let movies = this.state.dataSource.map((val, key) => {
+                return (
+                    <View key={key}>
+                        <Text>{val.main.temp}</Text>
+                    </View>
+                )
+            })*/
+
         return (
             <View>
                 <Text>Homepage</Text>
+                <Text>{temp}</Text>
+                <Text>{humidity}</Text>
             </View>
         )
+    }
     }
 }
 
